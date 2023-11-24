e79756fcc4d559bd2ebb53e9dd114c5c3e3d780c
Mhyst05 <81719787+Mhyst05@users.noreply.github.com>
2023-11-24 13:21:25 +0700

    Delete useless directory

--- .dhiwise/source/src/service/api.js
+++
@@ -1,8 +0,0 @@
-import { apis } from "service";
-
-const COMMON_URL = `https://nodedemo.dhiwise.co/device/auth/`;
-
-const API_URLS = { LOGIN_DEVICE_AUTH: `${COMMON_URL}login` };
-
-export const loginDeviceAuth = (payload) =>
-  apis.post(API_URLS.LOGIN_DEVICE_AUTH, payload);
